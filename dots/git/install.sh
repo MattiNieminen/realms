45b64ca05bedc31ff64ef5b1ec9181151a6717cb
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2023-12-12 19:33:45 +0200

    Add dots for Git

---
+++ dots/git/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+git config --global user.name "Matti Nieminen"
+git config --global user.email "MattiNieminen@users.noreply.github.com"
+git config --global credential.helper store
+git config --global pull.rebase true
